cfce3c15e59249f01c23aa9e4b151e3732fe47cf
DimaMakarenko <makarenko.d.viri@gmail.com>
2022-04-08 10:13:51 +0300

    init redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
+import { store } from 'reducers';
 
 import App from './App';
 
 render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/reducers/fileReducer.tsx
@@ -0,0 +1,8 @@
+const defaultState = {};
+
+export default function fileReducer(state = defaultState, action: { type: string }) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
