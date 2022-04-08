881654d509582e1832e2e679d8388753ea25c26a
DimaMakarenko <makarenko.d.viri@gmail.com>
2022-04-08 12:30:10 +0300

    init tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-function App() {
+export function App() {
   return (
     <div>
-      <h1>Hello there!</h1>
+      <h1 className="text-3xl font-bold">Hello there!</h1>
     </div>
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { render } from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import { Provider } from 'react-redux';
 
+import './index.css';
 import { store } from 'reducers';
 
-import App from './App';
+import { App } from './App';
 
-render(
+const container = document.getElementById('root') as HTMLElement;
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
